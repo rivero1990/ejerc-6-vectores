1e0dbdf84cc57ef8221d4587a553f240c4586962
Anibal Rivero <riveroleonel@gmail.com>
2023-05-28 19:09:42 -0300

    ejercicio 6 vectores

--- ejercicio6.js
+++ ejercicio6.js
@@ -2,7 +2,6 @@ const palabras = ["Manzana", "Banana", "Naranja", "Pera", "Uva"];
 
 const lista = document.querySelector("#frutas");
 
-let palabrasHtml = "";
 
 for (let i = 0; i < palabras.length; i++) {
     lista.innerHTML += `<li>${palabras[i]}</li>`;
